0a6d5fe5421fba604da1c96ed511278dd75fd076
AnRimkus <an_rimkus@INBOX.LV>
2018-06-22 15:26:46 +0300

    Server cannot access application directory 'C:\Scriptsara\wikiHowFanPage.html\'. The directory does not exist or is not accessible because of security settings

--- vmdsciis/DSC/dsciisdscext.ps1
+++ vmdsciis/DSC/dsciisdscext.ps1
@@ -2,21 +2,22 @@ Configuration Main
 {
 
 Param ( 
-    [string] $nodeName,
-    
-        # Name of the website to create
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [String]$WebSiteName,
-        # Source Path for Website content
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [String]$SourcePath,
-        # Destination path for Website content
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [String]$DestinationPath
-
+	[string] $nodeName,
+	
+		# Name of the website to create
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[String]$WebSiteName,
+		# Source Path for Website content
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[String]$SourcePath
+		
+	<# Destination path for Website content
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[String]$DestinationPath
+	#>
 )
 
 Import-DscResource -ModuleName PSDesiredStateConfiguration
@@ -24,162 +25,164 @@ Import-DscResource -Module xWebAdministration
 
 Node $nodeName
   {
-      File CreateScriptsFolder {
-        Type = 'Directory'
-        DestinationPath = 'C:\Scriptsara'
-        Ensure = "Present"
-    }
-      
-      WindowsFeature WebServerRole
-    {
-      Name = "Web-Server"
-      Ensure = "Present"
-    }
+	  File CreateScriptsFolder {
+		Type = 'Directory'
+		DestinationPath = 'C:\Scriptsara'
+		Ensure = "Present"
+	}
+	  
+	  WindowsFeature WebServerRole
+	{
+	  Name = "Web-Server"
+	  Ensure = "Present"
+	}
 
-      
+	  
 
-          # Install the ASP .NET 4.5 role
-        WindowsFeature AspNet45
-        {
-            Ensure          = "Present"
-            Name            = "Web-Asp-Net45"
-        }
+		  # Install the ASP .NET 4.5 role
+		WindowsFeature AspNet45
+		{
+			Ensure          = "Present"
+			Name            = "Web-Asp-Net45"
+		}
 
-        # Stop the default website
-       
-      xWebsite DefaultSite
-        {
-            Ensure          = "Present"
-            Name            = "Default Web Site"
-            State           = "Stopped"
-            PhysicalPath    = "C:\inetpub\wwwroot"
-            DependsOn       = "[WindowsFeature]WebServerRole"
-        }
-      
-      <#
-        # Copy the website content
-        File WebContent
-        {
-            Ensure          = "Present"
-            SourcePath      = $SourcePath
-            DestinationPath = $DestinationPath
-            Recurse         = $true
-            Type            = "Directory"
-            DependsOn       = "[WindowsFeature]AspNet45"
-        }
-      #>
+		# Stop the default website
+	   
+	  xWebsite DefaultSite
+		{
+			Ensure          = "Present"
+			Name            = "Default Web Site"
+			State           = "Stopped"
+			PhysicalPath    = "C:\inetpub\wwwroot"
+			DependsOn       = "[WindowsFeature]WebServerRole"
+		}
+	  
+	  <#
+		# Copy the website content
+		File WebContent
+		{
+			Ensure          = "Present"
+			SourcePath      = $SourcePath
+			DestinationPath = $DestinationPath
+			Recurse         = $true
+			Type            = "Directory"
+			DependsOn       = "[WindowsFeature]AspNet45"
+		}
+	  #>
 
-        # Create the new Website with HTTPS
-        xWebsite NewWebsite
-        {
-            Ensure          = "Present"
-            Name            = $WebSiteName
-            State           = "Started"
-            PhysicalPath    = $DestinationPath
-            BindingInfo     = @(
-                MSFT_xWebBindingInformation
-                {
-                    Protocol              = "HTTPS"
-                    Port                  = 8443
-                    CertificateThumbprint = "71AD93562316F21F74606F1096B85D66289ED60F"
-                    CertificateStoreName  = "WebHosting"
-                }
-                MSFT_xWebBindingInformation
-                {
-                    Protocol              = "HTTPS"
-                    Port                  = 8444
-                    CertificateThumbprint = "DEDDD963B28095837F558FE14DA1FDEFB7FA9DA7"
-                    CertificateStoreName  = "MY"
-                }
-            )
-            #DependsOn       = "[File]WebContent"
+		# Create the new Website with HTTPS
+		xWebsite NewWebsite
+		{
+			Ensure          = "Present"
+			Name            = $WebSiteName
+			State           = "Started"
+			PhysicalPath    = "C:\Scriptsara"
+			BindingInfo     = @(
+				MSFT_xWebBindingInformation
+				{
+					Protocol              = "HTTP"
+					Port                  = 8443
+					#CertificateThumbprint = "71AD93562316F21F74606F1096B85D66289ED60F"
+					#CertificateStoreName  = "WebHosting"
+					HostName = "test1"
+				}
+				MSFT_xWebBindingInformation
+				{
+					Protocol              = "HTTP"
+					Port                  = 8444
+					#CertificateThumbprint = "DEDDD963B28095837F558FE14DA1FDEFB7FA9DA7"
+					#CertificateStoreName  = "MY"
+					HostName = "test2"
+				}
+			)
+			#DependsOn       = "[File]WebContent"
 
    <# This commented section represents an example configuration that can be updated as required.
-    
-    WindowsFeature WebManagementConsole
-    {
-      Name = "Web-Mgmt-Console"
-      Ensure = "Present"
-    }
-    WindowsFeature WebManagementService
-    {
-      Name = "Web-Mgmt-Service"
-      Ensure = "Present"
-    }
-    WindowsFeature ASPNet45
-    {
-      Name = "Web-Asp-Net45"
-      Ensure = "Present"
-    }
-    WindowsFeature HTTPRedirection
-    {
-      Name = "Web-Http-Redirect"
-      Ensure = "Present"
-    }
-    WindowsFeature CustomLogging
-    {
-      Name = "Web-Custom-Logging"
-      Ensure = "Present"
-    }
-    WindowsFeature LogginTools
-    {
-      Name = "Web-Log-Libraries"
-      Ensure = "Present"
-    }
-    WindowsFeature RequestMonitor
-    {
-      Name = "Web-Request-Monitor"
-      Ensure = "Present"
-    }
-    WindowsFeature Tracing
-    {
-      Name = "Web-Http-Tracing"
-      Ensure = "Present"
-    }
-    WindowsFeature BasicAuthentication
-    {
-      Name = "Web-Basic-Auth"
-      Ensure = "Present"
-    }
-    WindowsFeature WindowsAuthentication
-    {
-      Name = "Web-Windows-Auth"
-      Ensure = "Present"
-    }
-    WindowsFeature ApplicationInitialization
-    {
-      Name = "Web-AppInit"
-      Ensure = "Present"
-    }
-    Script DownloadWebDeploy
-    {
-        TestScript = {
-            Test-Path "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
-        }
-        SetScript ={
-            $source = "https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi"
-            $dest = "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
-            Invoke-WebRequest $source -OutFile $dest
-        }
-        GetScript = {@{Result = "DownloadWebDeploy"}}
-        DependsOn = "[WindowsFeature]WebServerRole"
-    }
-    Package InstallWebDeploy
-    {
-        Ensure = "Present"  
-        Path  = "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
-        Name = "Microsoft Web Deploy 3.6"
-        ProductId = "{ED4CC1E5-043E-4157-8452-B5E533FE2BA1}"
-        Arguments = "ADDLOCAL=ALL"
-        DependsOn = "[Script]DownloadWebDeploy"
-    }
-    Service StartWebDeploy
-    {                    
-        Name = "WMSVC"
-        StartupType = "Automatic"
-        State = "Running"
-        DependsOn = "[Package]InstallWebDeploy"
-    } #>
+	
+	WindowsFeature WebManagementConsole
+	{
+	  Name = "Web-Mgmt-Console"
+	  Ensure = "Present"
+	}
+	WindowsFeature WebManagementService
+	{
+	  Name = "Web-Mgmt-Service"
+	  Ensure = "Present"
+	}
+	WindowsFeature ASPNet45
+	{
+	  Name = "Web-Asp-Net45"
+	  Ensure = "Present"
+	}
+	WindowsFeature HTTPRedirection
+	{
+	  Name = "Web-Http-Redirect"
+	  Ensure = "Present"
+	}
+	WindowsFeature CustomLogging
+	{
+	  Name = "Web-Custom-Logging"
+	  Ensure = "Present"
+	}
+	WindowsFeature LogginTools
+	{
+	  Name = "Web-Log-Libraries"
+	  Ensure = "Present"
+	}
+	WindowsFeature RequestMonitor
+	{
+	  Name = "Web-Request-Monitor"
+	  Ensure = "Present"
+	}
+	WindowsFeature Tracing
+	{
+	  Name = "Web-Http-Tracing"
+	  Ensure = "Present"
+	}
+	WindowsFeature BasicAuthentication
+	{
+	  Name = "Web-Basic-Auth"
+	  Ensure = "Present"
+	}
+	WindowsFeature WindowsAuthentication
+	{
+	  Name = "Web-Windows-Auth"
+	  Ensure = "Present"
+	}
+	WindowsFeature ApplicationInitialization
+	{
+	  Name = "Web-AppInit"
+	  Ensure = "Present"
+	}
+	Script DownloadWebDeploy
+	{
+		TestScript = {
+			Test-Path "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
+		}
+		SetScript ={
+			$source = "https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi"
+			$dest = "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
+			Invoke-WebRequest $source -OutFile $dest
+		}
+		GetScript = {@{Result = "DownloadWebDeploy"}}
+		DependsOn = "[WindowsFeature]WebServerRole"
+	}
+	Package InstallWebDeploy
+	{
+		Ensure = "Present"  
+		Path  = "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
+		Name = "Microsoft Web Deploy 3.6"
+		ProductId = "{ED4CC1E5-043E-4157-8452-B5E533FE2BA1}"
+		Arguments = "ADDLOCAL=ALL"
+		DependsOn = "[Script]DownloadWebDeploy"
+	}
+	Service StartWebDeploy
+	{                    
+		Name = "WMSVC"
+		StartupType = "Automatic"
+		State = "Running"
+		DependsOn = "[Package]InstallWebDeploy"
+	} #>
   }
 }
-    }
+	}
